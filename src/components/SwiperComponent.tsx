b8263a7a2b67d80bf7b55cfe8c90277361844c1e
Vasco <vasferraro@gmail.com>
2026-01-03 17:05:19 +0000

    add swiper breakpoints

--- src/components/SwiperComponent.tsx
+++ src/components/SwiperComponent.tsx
@@ -13,9 +13,23 @@ const SwiperComponent = ({ slides }: SwiperComponentProps) => {
   return (
     <Swiper
       modules={[Pagination]}
-      spaceBetween={50}
+      spaceBetween={10}
       slidesPerView={1}
       pagination={{ clickable: true }}
+      breakpoints={{
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      }}
     >
       {slides.map((slide) => (
         <SwiperSlide key={slide.title}>
