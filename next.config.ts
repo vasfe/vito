55ab225fc895c4d0f5480c4932ce2b7ca4969949
Vasco <vasferraro@gmail.com>
2026-01-06 19:52:10 +0000

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "/vito",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   reactCompiler: true,
 };
 
